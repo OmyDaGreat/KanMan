e2c8ca4ba66039cd49287be35b336cfaabd0e21f
Om Gupta <ogupta4242@gmail.com>
2026-06-21 19:27:54 -0700

    feat(user): add separate public boards route mayhaps forgot to use auth filter when using currentUser function...

--- site/src/jvmMain/kotlin/xyz/malefic/kanman/user/UserRoutes.kt
+++ site/src/jvmMain/kotlin/xyz/malefic/kanman/user/UserRoutes.kt
@@ -4,21 +4,31 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.UNAUTHORIZED
 import org.http4k.routing.bind
-import xyz.malefic.kanman.auth.currentUser
+import xyz.malefic.kanman.data.model.Visibility
+import xyz.malefic.kanman.util.authRequest
 import xyz.malefic.kanman.util.catch
+import xyz.malefic.kanman.util.catchPlus
 import xyz.malefic.kanman.util.error
 import xyz.malefic.kanman.util.response
 import xyz.malefic.kanman.util.toVisibility
 
 val userRoutes =
     arrayOf(
-        "/api/boards" bind GET to
-            catch("Failed to list boards") { request ->
-                val visibility = request.query("visibility")?.toVisibility
-                val user = currentUser(request)
-                val boards =
-                    getUserBoards(visibility, user) ?: return@catch error(UNAUTHORIZED) { "Authentication required for private boards" }
+        "/api/boards/public" bind GET to
+            catch("Failed to list public boards") {
+                val boards = getUserBoards(Visibility.PUBLIC, null)!!
 
                 response(OK, boards)
+            },
+        "/api/boards" bind GET to
+            catchPlus("Failed to list boards") {
+                authRequest { user ->
+                    val visibility = query("visibility")?.toVisibility
+                    val boards =
+                        getUserBoards(visibility, user)
+                            ?: return@authRequest error(UNAUTHORIZED) { "Authentication required for private boards" }
+
+                    response(OK, boards)
+                }
             },
     )
