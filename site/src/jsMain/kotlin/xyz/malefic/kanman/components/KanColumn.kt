1511ca4b7342852bb9a4e377a57477bebef1ca0f
Om Gupta <ogupta4242@gmail.com>
2026-06-27 09:28:00 -0700

    feat(ui): random color sticky notes :D

--- site/src/jsMain/kotlin/xyz/malefic/kanman/components/KanColumn.kt
+++ site/src/jsMain/kotlin/xyz/malefic/kanman/components/KanColumn.kt
@@ -1,15 +1,20 @@
 package xyz.malefic.kanman.components
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.Overflow
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.aspectRatio
 import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxHeight
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.overflow
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.layout.Surface
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.vw
@@ -19,21 +24,29 @@ import org.jetbrains.compose.web.dom.Text
 import xyz.malefic.kanman.styles.Colors
 
 @Composable
-fun KanColumn(title: String) =
+fun KanColumn(column: String) =
     Column(
         Modifier
             .fillMaxHeight()
             .width(20.vw)
             .backgroundColor(Colors.primaryContainer)
             .padding(18.px)
-            .borderRadius(18.px),
+            .borderRadius(18.px)
+            .overflow(Overflow.Scroll),
         Arrangement.Top,
     ) {
         H2 {
-            Text(title)
+            Text(column)
         }
-        Surface(Modifier.fillMaxWidth()) {
-            P {
+        val stickyColor = listOf(Colors.tertiary, Colors.error, Colors.primary, Colors.secondary)
+        Surface(
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(1)
+                .backgroundColor(stickyColor.random())
+                .padding(18.px),
+        ) {
+            P(Modifier.color(Colors.onTertiary).overflow(Overflow.Scroll).toAttrs()) {
                 Text(
                     "this is content the most content more content than any other content no content does content like this content does content",
                 )
