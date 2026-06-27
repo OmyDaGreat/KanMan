fc321b5c8edbfcf76fb1b16b02739184fc0dd1f2
Om Gupta <ogupta4242@gmail.com>
2026-06-27 13:39:14 -0700

    fix(ui): mayhaps forgot to update the home page cool request stuff from last update yes create blank login page

--- site/src/jsMain/kotlin/xyz/malefic/kanman/pages/Index.kt
+++ site/src/jsMain/kotlin/xyz/malefic/kanman/pages/Index.kt
@@ -1,35 +1,25 @@
 package xyz.malefic.kanman.pages
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
-import arrow.core.getOrElse
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.navigation.Link
 import xyz.malefic.kanman.api.board
+import xyz.malefic.kanman.api.util.Request
 import xyz.malefic.kanman.data.model.BoardCreateModel
-import xyz.malefic.kanman.data.model.BoardResponseModel
 import xyz.malefic.kanman.data.model.Visibility
 
 @Page
 @Composable
 fun HomePage() {
-    val board by produceState<BoardResponseModel?>(initialValue = null) {
-        value = board(BoardCreateModel("Test", Visibility.PUBLIC)).getOrElse { null }
-    }
-
-    if (board == null) {
-        // TODO: Handle loading state
-        return
-    }
-
-    Box(Modifier.fillMaxSize()) {
-        Link(
-            path = "/board/${board!!.id}",
-            text = "Go to Board: ${board!!.title}",
-        )
+    Request(request = { board(BoardCreateModel("Test", Visibility.PUBLIC)) }) { board ->
+        Box(Modifier.fillMaxSize()) {
+            Link(
+                path = "/board/${board.id}",
+                text = "Go to Board: ${board.title}",
+            )
+        }
     }
 }
---
+++ site/src/jsMain/kotlin/xyz/malefic/kanman/pages/Login.kt
@@ -0,0 +1,9 @@
+package xyz.malefic.kanman.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun LoginPage() {
+}
